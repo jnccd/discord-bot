02733234be1f1046603b03d42c28f25b7f23b047
jnccd <kobidogao@outlook.com>
2018-09-13 15:06:31 +0200

    Projektdateien hinzufügen.

---
+++ TestDiscordBot/Commands/Anime.cs
@@ -0,0 +1,46 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDiscordBot.Commands
+{
+    public class Anime : Command
+    {
+        public Anime() : base("animeQuote", false)
+        {
+
+        }
+
+        public override async Task execute(SocketMessage commandmessage)
+        {
+            try
+            {
+                string[] Files = Directory.GetFiles(@"D:\Eigene Dateien\Medien\Bilder\Reactions\Anime Quote");
+                List<string> SendableFiles = new List<string>();
+                foreach (string s in Files)
+                {
+                    if (Path.GetExtension(s) == ".jpg" || Path.GetExtension(s) == ".png" || Path.GetExtension(s) == ".jpeg" ||
+                        Path.GetExtension(s) == ".gif" || Path.GetExtension(s) == ".mp4")
+                        SendableFiles.Add(s);
+                }
+                string filepath = SendableFiles[Global.RDM.Next(SendableFiles.Count)];
+                await Global.SendFile(filepath, commandmessage.Channel);
+                Console.CursorLeft = 0;
+                Console.WriteLine("Send anime quote in " + commandmessage.Channel.Name + " for " + commandmessage.Author.Username + " from " + filepath);
+                Console.Write("$");
+            }
+            catch (Exception e)
+            {
+                Console.CursorLeft = 0;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("$");
+            }
+        }
+    }
+}
