2718ea642f54fabc49e3f9e0505537b6f61e37fb
jnccd <kobidogao@outlook.com>
2018-10-08 15:38:35 +0200

    Added error messages to every command Added 9ball command

--- TestDiscordBot/Commands/Ball.cs
+++ TestDiscordBot/Commands/Ball.cs
@@ -1,16 +1,17 @@
 ﻿using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestDiscordBot.Commands
 {
-    public class aaaTemplate : Command
+    public class Ball : Command
     {
-        public aaaTemplate() : base("", false)
+        string[] answers = new string[] { "NO!", "YES!", "No", "Yes", "Maybe", "Ask my wife", "Ask 8ball",  };
+
+        public Ball() : base("9ball", false)
         {
 
         }
@@ -19,10 +20,16 @@ namespace TestDiscordBot.Commands
         {
             try
             {
-                await Global.SendText("no u", commandmessage.Channel);
+                string m = commandmessage.Content;
+                long sum = 0;
+                for (int i = 0; i < m.Length; i++)
+                    sum += m.ToCharArray()[i] << i;
+
+                int answerIndex = (int)(sum % answers.Length);
+                await Global.SendText(answers[answerIndex], commandmessage.Channel);
 
                 Console.CursorLeft = 0;
-                Console.WriteLine("Literally nothing in " + commandmessage.Channel.Name + " for " + commandmessage.Author.Username);
+                Console.WriteLine("Send 9ball in " + commandmessage.Channel.Name + " for " + commandmessage.Author.Username);
                 Console.Write("$");
             }
             catch (Exception e)
