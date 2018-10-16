3ec29cf4e86568c91bb0f51bf0cd12ab4ee085be
Niklas Carstensen <creeper99.mc@gmail.com>
2018-10-16 11:42:04 +0200

    Added !profilePicture command

---
+++ TestDiscordBot/Commands/ProfilePicture.cs
@@ -0,0 +1,46 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDiscordBot.Commands
+{
+    public class ProfilePicture : Command
+    {
+        public ProfilePicture() : base("profilePicture", "Gets the profile picture of a mentioned target or yourself if you dont mention anyone", false)
+        {
+
+        }
+
+        public override async Task execute(SocketMessage commandmessage)
+        {
+            try
+            {
+                SocketUser Target;
+
+                if (commandmessage.MentionedUsers.Count > 0)
+                    Target = commandmessage.MentionedUsers.ElementAt(0);
+                else
+                    Target = commandmessage.Author;
+
+                await Global.SendText(Target.GetAvatarUrl(), commandmessage.Channel);
+
+                Console.CursorLeft = 0;
+                Console.WriteLine("Profile picture in " + commandmessage.Channel.Name + " for " + commandmessage.Author.Username);
+                Console.Write("$");
+            }
+            catch (Exception e)
+            {
+                await Global.SendText("Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!", commandmessage.Channel);
+
+                Console.CursorLeft = 0;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("$");
+            }
+        }
+    }
+}
