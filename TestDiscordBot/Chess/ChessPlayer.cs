eac3b03227d9d34b6d20218764ce1bf6164efdcd
jnccd <kobidogao@outlook.com>
2018-10-20 04:26:58 +0200

    Imported Chess Classes

---
+++ TestDiscordBot/Chess/ChessPlayer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace XNAChessAI
+{
+    public class ChessPlayer
+    {
+        [XmlIgnore]
+        public ChessBoard Parent;
+
+        public ChessPlayer()
+        {
+            Parent = null;
+        }
+        public ChessPlayer(ChessBoard Parent)
+        {
+            this.Parent = Parent;
+        }
+
+        public virtual void MovePiece(Point From, Point To)
+        {
+            Parent.MovePiece(From, To);
+        }
+
+        public virtual void NewMatchStarted(bool IsTopPlayer)
+        {
+
+        }
+        public virtual void TurnStarted()
+        {
+
+        }
+        public virtual void Update()
+        {
+
+        }
+        public virtual void Draw(SpriteBatch SB)
+        {
+
+        }
+    }
+}
