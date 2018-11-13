bda6b2438c9240a0982c21b400e476a72b13b824
jnccd <kobidogao@outlook.com>
2018-11-13 01:37:39 +0100

    Added $getTimestamp

--- TestDiscordBot/Commands/GetTimecode.cs
+++ TestDiscordBot/Commands/GetTimecode.cs
@@ -1,4 +1,5 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace TestDiscordBot.Commands
 {
     public class GetTimecode : Command
     {
-        public GetTimecode() : base("getTimecode", "", true, true)
+        public GetTimecode() : base("getTimestamp", "Gets the exact timestamp of a message", false)
         {
 
         }
@@ -17,7 +18,16 @@ namespace TestDiscordBot.Commands
         public override async Task execute(SocketMessage message)
         {
             string[] split = message.Content.Split(' ');
-            //if (split.Length)
+            if (split.Length == 1)
+                await Global.SendText("I need a messageID!", message.Channel);
+            else if (split.Length == 2)
+            {
+                string messageID = split[1].Split('/').Last();
+                IMessage m = await message.Channel.GetMessageAsync(Convert.ToUInt64(messageID));
+                await Global.SendText("Posted: " + m.Timestamp + "\nEdited: " + m.EditedTimestamp, message.Channel);
+            }
+            else
+                await Global.SendText("Thats too many parameters, I only need 2!", message.Channel);
         }
     }
 }
