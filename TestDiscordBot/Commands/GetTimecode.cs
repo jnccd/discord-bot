22fcd0e1151af7e82ce8b0f4a7981cbf0aeb01a1
jnccd <kobidogao@outlook.com>
2018-11-13 01:22:51 +0100

    Removed Leet Command Commands and prefixes mustn't contain spaces now Added reactions

---
+++ TestDiscordBot/Commands/GetTimecode.cs
@@ -0,0 +1,23 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDiscordBot.Commands
+{
+    public class GetTimecode : Command
+    {
+        public GetTimecode() : base("getTimecode", "", true, true)
+        {
+
+        }
+
+        public override async Task execute(SocketMessage message)
+        {
+            string[] split = message.Content.Split(' ');
+            //if (split.Length)
+        }
+    }
+}
