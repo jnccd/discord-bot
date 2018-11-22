96568c722538e3b914fe2680e308328b6eda05e3
Niklas Carstensen <creeper99.mc@gmail.com>
2018-11-22 21:54:24 +0100

    Added $urbanDict command Added command autocorrect

---
+++ TestDiscordBot/Commands/UrbanDict.cs
@@ -0,0 +1,32 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDiscordBot.Commands
+{
+    public class UrbanDict : Command
+    {
+        public UrbanDict() : base("urbanDict", "Search for a word you dont know.", true)
+        {
+
+        }
+
+        public override async Task execute(SocketMessage message)
+        {
+            string[] split = message.Content.Split(' ');
+
+            if (split.Length <= 1)
+            {
+                await Global.SendText("I need something to search!", message.Channel);
+                return;
+            }
+
+            string url = string.Format("https://www.urbandictionary.com/define.php?term=" + WebUtility.UrlEncode(string.Join(" ", split.Skip(1).ToArray())));
+            await Global.SendText(url, message.Channel);
+        }
+    }
+}
