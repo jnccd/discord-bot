7293b5cb3ce507d5cb28b4c9800b41c34da972d6
jnccd <kobidogao@outlook.com>
2018-12-11 13:22:06 +0100

    Added editLast Stir/Fall

--- TestDiscordBot/Commands/EditLast.cs
+++ TestDiscordBot/Commands/EditLast.cs
@@ -30,6 +30,9 @@ namespace TestDiscordBot.Commands
             new EditLastCommand("CAPS", "Convert text to CAPS", true, async (SocketMessage message, string lastText, string lastPic) => {
                 await Global.SendText(string.Join("", lastText.Select((x) => { return char.ToUpper(x); })), message.Channel);
             }),
+            new EditLastCommand("SUPERCAPS", "Convert text to SUPER CAPS", true, async (SocketMessage message, string lastText, string lastPic) => {
+                await Global.SendText(string.Join("", lastText.Select((x) => { return char.ToUpper(x) + " "; })), message.Channel);
+            }),
             new EditLastCommand("colorChannelSwap", "Swap the rgb color channels for each pixel", false, async (SocketMessage message, string lastText, string lastPic) => {
                 Bitmap bmp = Global.GetBitmapFromURL(lastPic);
 
@@ -63,7 +66,7 @@ namespace TestDiscordBot.Commands
                 for (int x = 0; x < bmp.Width; x++)
                     for (int y = 0; y < bmp.Height; y++)
                     {
-                        Vector2 target = new Vector2(x, y).RepelFrom(new Vector2(bmp.Width / 2, bmp.Height / 2), bmp, true);
+                        Vector2 target = Transform(new Vector2(x, y), new Vector2(bmp.Width / 2, bmp.Height / 2), bmp, TransformMode.Expand);
                         output.SetPixel(x, y, bmp.GetPixel((int)target.X, (int)target.Y));
                     }
 
@@ -76,7 +79,20 @@ namespace TestDiscordBot.Commands
                 for (int x = 0; x < bmp.Width; x++)
                     for (int y = 0; y < bmp.Height; y++)
                     {
-                        Vector2 target = new Vector2(x, y).RepelFrom(new Vector2(bmp.Width / 2, bmp.Height / 2), bmp, false);
+                        Vector2 target = Transform(new Vector2(x, y), new Vector2(bmp.Width / 2, bmp.Height / 2), bmp, TransformMode.Collapse);
+                        output.SetPixel(x, y, bmp.GetPixel((int)target.X, (int)target.Y));
+                    }
+
+                await Global.SendBitmap(output, message.Channel);
+            }),
+            new EditLastCommand("stir", "Stir the picture", false, async (SocketMessage message, string lastText, string lastPic) => {
+                Bitmap bmp = Global.GetBitmapFromURL(lastPic);
+                Bitmap output = new Bitmap(bmp.Width, bmp.Height);
+
+                for (int x = 0; x < bmp.Width; x++)
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        Vector2 target = Transform(new Vector2(x, y), new Vector2(bmp.Width / 2, bmp.Height / 2), bmp, TransformMode.Stir);
                         output.SetPixel(x, y, bmp.GetPixel((int)target.X, (int)target.Y));
                     }
 
@@ -85,7 +101,70 @@ namespace TestDiscordBot.Commands
 
             };
         }
-        
+        private static Vector2 Transform(Vector2 point, Vector2 center, Bitmap within, TransformMode mode)
+        {
+            Vector2 diff = point - center;
+            Vector2 move = diff;
+            move.Normalize();
+            
+            Vector2 target = Vector2.Zero;
+            float transformedLength = 0;
+            float rotationAngle = 0;
+            double cos = 0;
+            double sin = 0;
+            float maxDistance = (center / ((within.Width + within.Height) / 7)).LengthSquared();
+            switch (mode)
+            {
+                case TransformMode.Expand:
+                    transformedLength = (diff / ((within.Width + within.Height) / 7)).LengthSquared();
+                    target = (transformedLength == 0 || (100 / transformedLength) > diff.Length()) ?
+                        center :
+                        point - move * (100 / transformedLength);
+                    break;
+
+                case TransformMode.Collapse:
+                    transformedLength = (diff / ((within.Width + within.Height) / 7)).LengthSquared();
+                    target = (transformedLength == 0) ?
+                        center :
+                        point + move * (100 / transformedLength);
+                    break;
+
+                case TransformMode.Stir:
+                    transformedLength = (diff / ((within.Width + within.Height) / 7)).LengthSquared();
+                    rotationAngle = (float)Math.Pow((maxDistance - transformedLength), 5) / 10;
+                    cos = Math.Cos(rotationAngle);
+                    sin = Math.Sin(rotationAngle);
+                    target = new Vector2((float)(cos * (point.X - center.X) - sin * (point.Y - center.Y) + center.X), 
+                                         (float)(sin * (point.X - center.X) + cos * (point.Y - center.Y) + center.Y));
+                    break;
+
+                case TransformMode.Fall:
+                    transformedLength = (diff / ((within.Width + within.Height) / 7)).LengthSquared();
+                    rotationAngle = transformedLength / 3;
+                    cos = Math.Cos(rotationAngle);
+                    sin = Math.Sin(rotationAngle);
+                    target = new Vector2((float)(cos * (point.X - center.X) - sin * (point.Y - center.Y) + center.X),
+                                         (float)(sin * (point.X - center.X) + cos * (point.Y - center.Y) + center.Y));
+                    break;
+            }
+
+            if (float.IsNaN(target.X) || float.IsInfinity(target.X))
+                target.X = point.X;
+            if (float.IsNaN(target.Y) || float.IsInfinity(target.Y))
+                target.Y = point.Y;
+            if (target.X < 0)
+                target.X = 0;
+            if (target.X > within.Width - 1)
+                target.X = within.Width - 1;
+            if (target.Y < 0)
+                target.Y = 0;
+            if (target.Y > within.Height - 1)
+                target.Y = within.Height - 1;
+
+            return target;
+        }
+        private enum TransformMode { Expand, Collapse, Stir, Fall }
+
         public override async Task execute(SocketMessage message)
         {
             IEnumerable<IMessage> messages = await message.Channel.GetMessagesAsync().Flatten();
