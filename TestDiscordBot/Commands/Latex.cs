405718ba3db2e5cb3296233a4540acce1c1985ef
jnccd <kobidogao@outlook.com>
2019-03-27 21:43:41 +0100

    Gave $latex some background

--- TestDiscordBot/Commands/Latex.cs
+++ TestDiscordBot/Commands/Latex.cs
@@ -27,30 +27,38 @@ namespace TestDiscordBot.Commands
         {
             lock (this)
             {
-                try
+                string latex = message.Content.Split(new char[] { ' ', '\n' }).Skip(1).Aggregate((x, y) => x + " " + y);
+
+                if (!latex.StartsWith("\\documentclass["))
+                    latex = "\\documentclass[preview,border=12pt]{standalone}\n\\usepackage{amsmath}\n\\usepackage{tikz}\n\\begin{document}\n" + latex + "\n\\end{document}";
+
+                if (!File.Exists(inputPath))
+                    File.Create(inputPath).Dispose();
+                File.WriteAllText(inputPath, latex);
+
+                Process converter = new Process();
+                converter.StartInfo.FileName = batchPath;
+                converter.StartInfo.UseShellExecute = false;
+                converter.StartInfo.RedirectStandardInput = true;
+
+                converter.Start();
+                Thread.Sleep(500);
+                converter.StandardInput.WriteLine("return");
+                converter.WaitForExit();
+
+                Bitmap output = null;
+                using (Bitmap latexOutput = new Bitmap(outputPath))
                 {
-                    string latex = message.Content.Split(new char[] { ' ', '\n' }).Skip(1).Aggregate((x, y) => x + " " + y);
-
-                    if (!latex.StartsWith("\\documentclass["))
-                        latex = "\\documentclass[preview,border=12pt]{standalone}\n\\usepackage{amsmath}\n\\usepackage{tikz}\n\\begin{document}\n" + latex + "\n\\end{document}";
-                    
-                    if (!File.Exists(inputPath))
-                        File.Create(inputPath).Dispose();
-                    File.WriteAllText(inputPath, latex);
-
-                    Process converter = new Process();
-                    converter.StartInfo.FileName = batchPath;
-                    converter.StartInfo.UseShellExecute = false;
-                    converter.StartInfo.RedirectStandardInput = true;
-
-                    converter.Start();
-                    Thread.Sleep(500);
-                    converter.StandardInput.WriteLine("return");
-                    converter.WaitForExit();
-
-                    Global.SendFile(outputPath, message.Channel).Wait();
+                    output = new Bitmap(latexOutput.Width, latexOutput.Height);
+                    using (Graphics graphics = Graphics.FromImage(output))
+                    {
+                        graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, latexOutput.Width, latexOutput.Height));
+                        graphics.DrawImage(latexOutput, new Point(0, 0));
+                    }
                 }
-                catch (Exception e) { Global.ConsoleWriteLine(e.ToString(), ConsoleColor.Red); }
+
+                Global.SendBitmap(output, message.Channel).Wait();
+                output.Dispose();
             }
 
             return Task.FromResult(default(object));
