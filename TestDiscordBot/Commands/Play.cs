c7208c143580caf65bada38d8e6d50bad53ad127
Niklas Carstensen <creeper99.mc@gmail.com>
2019-04-06 22:47:00 +0200

    Add multithreading support to $play, Change external Exception langauge to English

--- TestDiscordBot/Commands/Play.cs
+++ TestDiscordBot/Commands/Play.cs
@@ -17,6 +17,15 @@ namespace TestDiscordBot
             HelpMenu = new EmbedBuilder();
             HelpMenu.WithTitle("Give it a YoutTube link and it'll work.");
         }
+        
+        class PlayInstance
+        {
+            public Task playThread;
+            public IAudioClient client;
+            public ISocketAudioChannel channel;
+            public string VideoPath;
+        }
+        List<PlayInstance> instances = new List<PlayInstance>();
 
         public override Task Execute(SocketMessage message)
         {
@@ -25,20 +34,54 @@ namespace TestDiscordBot
                 Program.SendEmbed(HelpMenu, message.Channel).Wait();
                 return Task.FromResult(default(object));
             }
+
+            SocketGuild g = Program.GetGuildFromChannel(message.Channel);
+            ISocketAudioChannel channel = g.VoiceChannels.FirstOrDefault(x => x.Users.Select(y => y.Id).Contains(message.Author.Id));
+
             string videoURL = message.Content.Split(' ')[1];
             if (!videoURL.StartsWith("https://www.youtube.com/watch?"))
             {
                 Program.SendText("That doesn't look like a youtube video link :thinking:", message.Channel).Wait();
                 return Task.FromResult(default(object));
             }
-            SocketGuild g = Program.GetGuildFromChannel(message.Channel);
-            ISocketAudioChannel channel = g.VoiceChannels.FirstOrDefault(x => x.Users.Select(y => y.Id).Contains(message.Author.Id));
+
             if (channel != null)
             {
-                string path = Program.DownloadVideoFromYouTube(videoURL);
-                IAudioClient client = channel.ConnectAsync().Result;
-                Program.SendAudioAsync(client, path).Wait();
-                channel.DisconnectAsync().Wait();
+                PlayInstance instance = instances.FirstOrDefault(x => x.channel.Id == channel.Id);
+                if (instance == null)
+                {
+                    Task thread;
+                    lock (this)
+                    {
+                        string path = Program.DownloadVideoFromYouTube(videoURL);
+                        IAudioClient client = channel.ConnectAsync().Result;
+                        thread = Program.SendAudioAsync(client, path);
+                        instance = new PlayInstance { playThread = thread, channel = channel, client = client, VideoPath = path };
+                        instances.Add(instance);
+                    }
+                    thread.Wait();
+                    channel.DisconnectAsync().Wait();
+                    instances.Remove(instance);
+                }
+                else
+                {
+                    Task thread;
+                    lock (this)
+                    {
+                        instance.channel.DisconnectAsync().Wait();
+                        instances.Remove(instance);
+
+                        string path = Program.DownloadVideoFromYouTube(videoURL);
+                        IAudioClient client = channel.ConnectAsync().Result;
+                        thread = Program.SendAudioAsync(client, path);
+                        instance = new PlayInstance { playThread = thread, channel = channel, client = client, VideoPath = path };
+                        instances.Add(instance);
+                    }
+
+                    thread.Wait();
+                    channel.DisconnectAsync().Wait();
+                    instances.Remove(instance);
+                }
             }
             else
             {
