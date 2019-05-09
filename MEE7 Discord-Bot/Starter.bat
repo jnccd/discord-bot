11ae721c17d0f8e11242093d538f06a1d2fc5216
jnccd <kobidogao@outlook.com>
2019-05-09 17:36:10 +0200

    Fix PatchNotes throwing errors when there are no commits

--- MEE7 Discord-Bot/Starter.bat
+++ MEE7 Discord-Bot/Starter.bat
@@ -1,3 +1,3 @@
 @echo off
 
-dotnet run
+dotnet run -c Release
---
+++ Starter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start(new ProcessStartInfo() {  });
+        }
+    }
+}
