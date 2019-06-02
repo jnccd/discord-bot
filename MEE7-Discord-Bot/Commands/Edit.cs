b9d7780e512a1068593bd8c072a34d05fa4a467b
niklasCarstensen <creeper99.mc@gmail.com>
2019-06-02 18:26:50 +0200

    Improve Command constructor

---
+++ MEE7-Discord-Bot/Commands/Edit.cs
@@ -0,0 +1,20 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEE7.Commands
+{
+    public class Edit : Command
+    {
+        public Edit() : base("edit", "Edit stuff")
+        {
+
+        }
+
+        public override void Execute(SocketMessage message)
+        {
+            
+        }
+    }
+}
