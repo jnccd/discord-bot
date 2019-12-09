6e85880b9ec9e6108ce05527dedf1675eede7465
jnccd <kobidogao@outlook.com>
2019-12-10 00:36:48 +0100

    Add loadConfig

---
+++ MEE7-Discord-Bot/Commands/LoadConfig.cs
@@ -0,0 +1,25 @@
+﻿using Discord.WebSocket;
+using MEE7.Backend;
+using MEE7.Backend.HelperFunctions.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MEE7.Commands
+{
+    public class LoadConfig : Command
+    {
+        public LoadConfig() : base("loadConfig", "loads the attached config", isExperimental: true, isHidden: true)
+        {
+
+        }
+
+        public override void Execute(SocketMessage message)
+        {
+            var url = message.Attachments.First().Url;
+            var json = url.GetHTMLfromURL();
+            Configuration.Config.LoadFrom(json);
+        }
+    }
+}
