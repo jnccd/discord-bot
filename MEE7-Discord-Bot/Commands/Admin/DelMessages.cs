053b12cf4d8e1e1d366d415db38a42c2e0dba23a
niklasCarstensen <creeper99.mc@gmail.com>
2020-01-10 18:45:51 +0100

    Add delMessages

---
+++ MEE7-Discord-Bot/Commands/Admin/DelMessages.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.WebSocket;
+using MEE7.Backend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEE7.Commands.Admin
+{
+    public class DelMessages : Command
+    {
+        public DelMessages() : base("delMessages", "Deletes n past messages", isExperimental: false, isHidden: true)
+        {
+
+        }
+
+        public override void Execute(SocketMessage message)
+        {
+            if (message.Author.Id == Program.Master.Id)
+            {
+                var amount = 1;
+                try { amount = int.Parse(message.Content.Split(' ')[1]); } catch { }
+
+                var messages = message.Channel.GetMessagesAsync(amount).FlattenAsync().Result;
+                foreach (var m in messages)
+                    m.DeleteAsync().Wait();
+            }
+        }
+    }
+}
