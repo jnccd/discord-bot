9010f9493ec65da023580bfa070b11062c6a7e2b
jnccd <kobidogao@outlook.com>
2020-03-13 15:39:01 +0100

    Fix Memory Leaks in Transground and Transcrop

--- MEE7-Discord-Bot/Backend/HelperFunctions/Extensions/DrawingExtensions.cs
+++ MEE7-Discord-Bot/Backend/HelperFunctions/Extensions/DrawingExtensions.cs
@@ -10,13 +10,9 @@ namespace MEE7.Backend.HelperFunctions.Extensions
     {
         public static Bitmap CropImage(this Bitmap source, Rectangle section)
         {
-            // An empty bitmap which will hold the cropped image
             Bitmap bmp = new Bitmap(section.Width, section.Height);
 
             using (Graphics g = Graphics.FromImage(bmp))
-
-                // Draw the given area (section) of the source image
-                // at location 0,0 on the empty bitmap (bmp)
                 g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
 
             return bmp;
