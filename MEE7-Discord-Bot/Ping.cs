9f1fd1e6c34290214476c8928c9267fc30be86bb
niklasCarstensen <creeper99.mc@gmail.com>
2020-04-14 18:59:41 +0200

    Add $ping

---
+++ MEE7-Discord-Bot/Ping.cs
@@ -0,0 +1,17 @@
+﻿using Discord.WebSocket;
+using MEE7.Backend;
+using MEE7.Backend.HelperFunctions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEE7
+{
+    class Ping : Command
+    {
+        public override void Execute(SocketMessage message)
+        {
+            DiscordNETWrapper.SendText($"Pong in {(int)(message.Timestamp - DateTime.Now).TotalMilliseconds}ms!", message.Channel).Wait();
+        }
+    }
+}
