a0eed0cfceb61162f1a4d653c7a0302c446daa6b
jnccd <kobidogao@outlook.com>
2020-04-16 01:59:47 +0200

    Add $ac

---
+++ MEE7-Discord-Bot/Commands/AC.cs
@@ -0,0 +1,13 @@
+﻿using Discord.WebSocket;
+using MEE7.Backend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEE7.Commands
+{
+    class AC : Command
+    {
+        public override void Execute(SocketMessage message) => new AnimalCrossing().Execute(message);
+    }
+}
