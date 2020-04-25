509a56499b27c9525d4386a96dea969b3a29b03a
niklasCarstensen <creeper99.mc@gmail.com>
2020-04-25 23:18:12 +0200

    Rework $animalCrossing

--- MEE7-Discord-Bot/Commands/AnimalCrossing.cs
+++ MEE7-Discord-Bot/Commands/AnimalCrossing.cs
@@ -3,6 +3,7 @@ using HtmlAgilityPack;
 using MEE7.Backend;
 using MEE7.Backend.HelperFunctions;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -37,17 +38,58 @@ namespace MEE7.Commands
             HtmlWeb web = new HtmlWeb();
             web.AutoDetectEncoding = false;
             web.OverrideEncoding = Encoding.UTF8;
-            var searchDoc = web.Load($"https://animalcrossingwiki.de/nachbarn/jens?do=search&sf=1&q={HttpUtility.UrlEncode(searchQuery)}+%40nachbarn");
-            string charLink;
+            IEnumerable<HtmlNode> searchHits = null;
             try
             {
-                var result = searchDoc.DocumentNode.SelectNodes("//a[contains(@class, 'wikilink1')]");
-                charLink = result.Skip(3).First().GetAttributeValue("href", "");
-                if (charLink == "")
-                    throw new Exception();
-                if (charLink.Contains("?"))
-                    charLink = charLink.GetEverythingBetween("", "?");
+                string searchURL = null;
+                if (searchQuery.Contains("@") || true)
+                    searchURL = $"https://animalcrossingwiki.de/nachbarn/jens?do=search&sf=1&q={HttpUtility.UrlEncode(searchQuery)}";
+                else
+                    searchURL = $"https://animalcrossingwiki.de/nachbarn/jens?do=search&sf=1&q={HttpUtility.UrlEncode(searchQuery)}+%40nachbarn";
 
+                var searchDoc = web.Load(searchURL);
+                searchHits = searchDoc.DocumentNode.SelectNodes("//a[contains(@class, 'wikilink1')]");
+            } 
+            catch
+            {
+                DiscordNETWrapper.SendText("Error occured while using the search page", message.Channel).Wait();
+                return;
+            }
+            searchHits = searchHits.Skip(3);
+
+            while (searchHits.Count() > 0)
+            {
+                string hit = searchHits.First().GetAttributeValue("href", "");
+                if (hit.Contains("?"))
+                    hit = hit.GetEverythingBetween("", "?");
+                if (hit == "")
+                    continue;
+                if (hit.StartsWith("/nachbarn"))
+                {
+                    germanWikiNeighbor(hit, web, message);
+                    return;
+                }
+                else if (hit.StartsWith("/acnh/katalog/umgestaltung/"))
+                {
+                    germanWikiKatalog(hit, web, message);
+                    return;
+                }
+                else if (hit.StartsWith("/acnh/katalog/tapeten"))
+                {
+                    germanWikiTapeten(hit, web, message, searchQuery);
+                    return;
+                }
+
+                searchHits = searchHits.Skip(1);
+            }
+
+            DiscordNETWrapper.SendText("Couldn't find anything", message.Channel).Wait();
+            return;
+        }
+        void germanWikiNeighbor(string charLink, HtmlWeb web, SocketMessage message)
+        {
+            try
+            {
                 var charDoc = web.Load("https://animalcrossingwiki.de" + charLink);
 
                 var table = charDoc.DocumentNode.SelectSingleNode("//table[contains(@class, 'inline')]");
@@ -70,24 +112,90 @@ namespace MEE7.Commands
                 var namesTable = otherNamesTitle.NextSibling.NextSibling.ChildNodes.ElementAt(1).FirstChild;
                 var nameEntries = namesTable.ChildNodes.Where(x => x.Name == "tr");
                 string names = nameEntries.Select(x => x.ChildNodes.ElementAt(1).InnerText + " - " + x.ChildNodes.ElementAt(2).InnerText).Combine("\n");
+                names = names.Replace("Englisch", ":flag_gb: Englisch").
+                              Replace("Japanisch", ":flag_jp: Japanisch").
+                              Replace("Spanisch", ":flag_es: Spanisch").
+                              Replace("Französisch", ":flag_fr: Französisch").
+                              Replace("Italienisch", ":flag_it: Italienisch");
 
                 DiscordNETWrapper.SendEmbed(DiscordNETWrapper.
-                    CreateEmbedBuilder(charName, names, charImg, null, "").
-                    AddFieldDirectly("Tierart", HttpUtility.HtmlDecode(tierart)).
-                    AddFieldDirectly("Persönlichkeit", HttpUtility.HtmlDecode(personality)).
-                    AddFieldDirectly("Geschlecht", HttpUtility.HtmlDecode(gender)).
-                    AddFieldDirectly("Geburtstag", HttpUtility.HtmlDecode(birthday)).
-                    AddFieldDirectly("Floskel", HttpUtility.HtmlDecode(floskel)).
-                    AddFieldDirectly("Fotospruch", HttpUtility.HtmlDecode(fotospruch)).
-                    AddFieldDirectly("Auftreten", HttpUtility.HtmlDecode(auftreten)),
+                    CreateEmbedBuilder(charName, $"[page link]({"https://animalcrossingwiki.de" + charLink})\n\n{names}", charImg, null, "").
+                    AddFieldDirectly("Tierart", HttpUtility.HtmlDecode(tierart), true).
+                    AddFieldDirectly("Persönlichkeit", HttpUtility.HtmlDecode(personality), true).
+                    AddFieldDirectly("Geschlecht", HttpUtility.HtmlDecode(gender), true).
+                    AddFieldDirectly("Geburtstag", HttpUtility.HtmlDecode(birthday), true).
+                    AddFieldDirectly("Floskel", HttpUtility.HtmlDecode(floskel), true).
+                    AddFieldDirectly("Fotospruch", HttpUtility.HtmlDecode(fotospruch), true).
+                    AddFieldDirectly("Auftreten", HttpUtility.HtmlDecode(auftreten), true),
                     message.Channel).Wait();
             }
             catch
             {
-                DiscordNETWrapper.SendText("Couldn't find that villager", message.Channel).Wait();
+                DiscordNETWrapper.SendText("Error parsing the villager page", message.Channel).Wait();
                 return;
             }
         }
+        void germanWikiKatalog(string link, HtmlWeb web, SocketMessage message)
+        {
+            try
+            {
+                var doc = web.Load("https://animalcrossingwiki.de" + link);
+
+                var title = doc.DocumentNode.SelectNodes("//article/h1").First().InnerText;
+                var image = doc.DocumentNode.SelectNodes("//img[contains(@class, 'media')]").ElementAt(1);
+                var imgLink = image.GetAttributeValue("src", "");
+
+                DiscordNETWrapper.SendEmbed(DiscordNETWrapper.
+                    CreateEmbedBuilder("", $"[{title}]({"https://animalcrossingwiki.de" + link})", "https://animalcrossingwiki.de" + imgLink, null, ""),
+                    message.Channel).Wait();
+            }
+            catch
+            {
+                DiscordNETWrapper.SendText("Error parsing the furniture page", message.Channel).Wait();
+                return;
+            }
+        }
+        void germanWikiTapeten(string link, HtmlWeb web, SocketMessage message, string searchQuery)
+        {
+            try
+            {
+                var doc = web.Load("https://animalcrossingwiki.de" + link);
+
+                var tapets = doc.DocumentNode.SelectNodes("//tr");
+                var searchIndex = tapets.
+                    Select(x => {
+                        string name = "";
+                        try
+                        {
+                            name = x.InnerHtml.GetEverythingBetween("<strong>", "</strong>");
+                        }
+                        catch { }
+                        return new Tuple<HtmlNode, string>(x, name);
+                    }).
+                    Where(x => !string.IsNullOrWhiteSpace(x.Item2) && 
+                               !x.Item2.StartsWith("Kaufpreis") &&
+                               !x.Item2.StartsWith("Verkaufspreis") &&
+                               !x.Item2.StartsWith("Quelle")).
+                    ToArray();
+                var searchRes = searchIndex.
+                    MinElement(x => searchQuery.ModifiedLevenshteinDistance(x.Item2));
+
+                var searchResName = searchRes.Item2;
+                var searchResIndex = tapets.IndexOf(searchRes.Item1);
+                var imgEntry = tapets[searchResIndex + 1];
+                var img = imgEntry.InnerHtml.GetEverythingBetween("src=\"", "\"");
+
+                DiscordNETWrapper.SendEmbed(DiscordNETWrapper.
+                    CreateEmbedBuilder(searchResName, "", "https://animalcrossingwiki.de" + img, null, ""),
+                    message.Channel).Wait();
+            }
+            catch
+            {
+                DiscordNETWrapper.SendText("Error parsing the tapeten page", message.Channel).Wait();
+                return;
+            }
+        }
+
 
         void englishWikiQuery(string searchQuery, SocketMessage message)
         {
