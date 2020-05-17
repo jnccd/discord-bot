88a01b55a0dca2949ae6ab4a7522c28eea8131d6
niklasCarstensen <niklas.carstensen@gmx.de>
2020-05-17 16:47:17 +0200

    Add EditShortcut

---
+++ MEE7-Discord-Bot/Commands/EditShortcut.cs
@@ -0,0 +1,18 @@
+﻿using Discord.WebSocket;
+using MEE7.Backend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEE7.Commands
+{
+    class EditShortcut : Command
+    {
+        public EditShortcut() : base("-", "Shortcut to the edit command", isExperimental: false, isHidden: true)
+        {
+
+        }
+
+        public override void Execute(SocketMessage message) => new Edit().Execute(message);
+    }
+}
