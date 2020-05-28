ed993710a2b64fc3a6d13308a8aaebb007510d6c
niklasCarstensen <niklas.carstensen@gmx.de>
2020-05-28 16:45:01 +0200

    Add AddStudentRole

---
+++ MEE7-Discord-Bot/Commands/CAUServerSpecific/AddStudentRole.cs
@@ -0,0 +1,28 @@
+﻿using Discord.WebSocket;
+using MEE7.Backend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MEE7.Commands.CAUServerSpecific
+{
+    public class AddStudentRole : Command
+    {
+        public AddStudentRole() : base("", "", false, true)
+        {
+            Program.OnConnected += Program_OnConnected;
+        }
+
+        private void Program_OnConnected()
+        {
+            SocketGuild uniServer = Program.GetGuildFromID(479950092938248193);
+            SocketRole studentRole = uniServer.GetRole(479953406299996180);
+            Program.OnUserJoined += (SocketGuildUser user) => user.AddRoleAsync(studentRole).Wait();
+        }
+
+        public override void Execute(SocketMessage message)
+        {
+
+        }
+    }
+}
