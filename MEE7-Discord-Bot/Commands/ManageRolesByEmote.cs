d14cd5e605b569118839088fcc3d084506bc78cb
jnccd <kobidogao@outlook.com>
2020-06-11 21:41:36 +0200

    Improve $sendRoleEmojiMessage

--- MEE7-Discord-Bot/Commands/ManageRolesByEmote.cs
+++ MEE7-Discord-Bot/Commands/ManageRolesByEmote.cs
@@ -7,6 +7,7 @@ using MEE7.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MEE7.Commands
 {
@@ -23,47 +24,53 @@ namespace MEE7.Commands
 
     public class ManageRolesByEmote : Command
     {
-        public ManageRolesByEmote() : base("sendRoleEmojiMessage", "", isExperimental: false, isHidden: false)
+        public ManageRolesByEmote() : base("sendRoleEmojiMessage", "", isExperimental: false, isHidden: true)
         {
             Program.OnEmojiReactionAdded += OnEmojiReactionAdded;
             Program.OnEmojiReactionRemoved += OnEmojiReactionRemoved;
+            Program.OnMessageDeleted += Program_OnMessageDeleted;
+        }
+
+        private void Program_OnMessageDeleted(Cacheable<IMessage, ulong> arg1, ISocketMessageChannel arg2)
+        {
+            for (int i = 0; i < Config.Data.manageRoleByEmoteMessages.Count; i++)
+                if (Config.Data.manageRoleByEmoteMessages[i].MessageID == arg1.Id &&
+                    Config.Data.manageRoleByEmoteMessages[i].ChannelID == arg2.Id)
+                    Config.Data.manageRoleByEmoteMessages.RemoveAt(i--);
         }
 
         private void OnEmojiReactionAdded(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
         {
             foreach (var m in Config.Data.manageRoleByEmoteMessages)
-                foreach (var t in m.EmoteRoleTuples)
-                    if (arg3.Emote.Name == t.Item1.Name)
-                    {
-                        var u = arg3.User.GetValueOrDefault();
-                        var g = Program.GetGuildFromChannel(arg2);
-                        g.Users.First(x => x.Id == u.Id).AddRoleAsync(g.Roles.First(x => x.Id == t.Item2));
-                    }
+                if (arg1.Id == m.MessageID)
+                    foreach (var t in m.EmoteRoleTuples)
+                        if (arg3.Emote.Name == t.Item1.Name)
+                        {
+                            var u = arg3.User.GetValueOrDefault();
+                            var g = Program.GetGuildFromChannel(arg2);
+                            g.Users.First(x => x.Id == u.Id).AddRoleAsync(g.Roles.First(x => x.Id == t.Item2));
+                        }
         }
 
         private void OnEmojiReactionRemoved(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
         {
             foreach (var m in Config.Data.manageRoleByEmoteMessages)
-                foreach (var t in m.EmoteRoleTuples)
-                    if (arg3.Emote.Name == t.Item1.Name)
-                    {
-                        var u = arg3.User.GetValueOrDefault();
-                        var g = Program.GetGuildFromChannel(arg2);
-                        g.Users.First(x => x.Id == u.Id).RemoveRoleAsync(g.Roles.First(x => x.Id == t.Item2));
-                    }
+                if (arg1.Id == m.MessageID)
+                    foreach (var t in m.EmoteRoleTuples)
+                        if (arg3.Emote.Name == t.Item1.Name)
+                        {
+                            var u = arg3.User.GetValueOrDefault();
+                            var g = Program.GetGuildFromChannel(arg2);
+                            g.Users.First(x => x.Id == u.Id).RemoveRoleAsync(g.Roles.First(x => x.Id == t.Item2));
+                        }
         }
 
         public override void Execute(IMessage message)
         {
-            //if (Program.GetGuildFromChannel(message.Channel).Roles.
-            //    FirstOrDefault(x => x.Permissions.ManageRoles && x.Members.FirstOrDefault(y => y.Id == message.Author.Id) != null) == null)
-            //{
-            //    DiscordNETWrapper.SendText("You don't have permissions to give roles!", message.Channel).Wait();
-            //    return;
-            //}
-
             var g = Program.GetGuildFromChannel(message.Channel);
-            if (message.Author.Id != g.Owner.Id && message.Author.Id != Program.Master.Id)
+            if (message.Author.Id != g.Owner.Id && message.Author.Id != Program.Master.Id 
+                && g.Roles.Where(x => x.Members.FirstOrDefault(x => x.Id == message.Author.Id) != null).Select(x => x.Position).Max() > 
+                   g.Roles.Where(x => x.Members.FirstOrDefault(x => x.Id == Program.GetSelf().Id) != null).Select(x => x.Position).Max())
             {
                 DiscordNETWrapper.SendText("You don't have permissions to use this command!", message.Channel).Wait();
                 return;
@@ -84,15 +91,26 @@ namespace MEE7.Commands
                     if (s.Length != 2)
                         throw new Exception(s.Combine(" "));
 
-                    if (Emote.TryParse(s[0], out Emote et))
-                        e = et;
-                    else
-                        e = new Emoji(s[0]);
+                    try
+                    {
+                        if (Emote.TryParse(s[0], out Emote et))
+                            e = et;
+                        else
+                            e = new Emoji(s[0]);
 
-                    r = roles.First(x => x.Name == s[1] || x.Id.ToString() == s[1]);
+                        r = roles.First(x => x.Name == s[1] || x.Id.ToString() == s[1]);
+                    } 
+                    catch
+                    {
+                        DiscordNETWrapper.SendText("Can't find role and/or emote " + s.Combine(" "), message.Channel).Wait();
+                        return null;
+                    }
 
                     return new Tuple<DiscordEmote, ulong>(DiscordEmote.FromIEmote(e), r.Id);
                 }).ToList();
+
+            if (emoteRoleTuples.Contains(null))
+                return;
 
             var roleMessage = DiscordNETWrapper.SendText("To get a role simply react to this message with the corresponding emote,\n" +
                 "to remove a role simply remove your reaction to this message.\n\n Emotes are mapped as follows:\n" +
