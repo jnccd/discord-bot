9f4319de0da5759ca9fe740bc05b8ed5da610530
niklasCarstensen <niklas.carstensen@gmx.de>
2020-08-07 22:04:50 +0200

    Add $- BakaMitai

--- MEE7-Discord-Bot/Commands/Edit/PictureCommands.cs
+++ MEE7-Discord-Bot/Commands/Edit/PictureCommands.cs
@@ -1,14 +1,18 @@
 ﻿using AnimatedGif;
 using BumpKit;
 using Discord;
+using IronPython.Hosting;
 using MEE7.Backend.HelperFunctions;
+using Microsoft.Scripting.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Threading;
 using static MEE7.Commands.Edit.Edit;
 using Color = System.Drawing.Color;
 using ImageFormat = System.Drawing.Imaging.ImageFormat;
@@ -85,6 +89,40 @@ namespace MEE7.Commands.Edit
                 }
 
                 return output;
+            }
+        }
+
+        public object firstOrderModelLock = new object();
+        public string BakaMitaiDesc = "Picture goes Baka Mitai - using code from https://github.com/AliaksandrSiarohin/first-order-model";
+        public void BakaMitai(Bitmap bmp, IMessage m)
+        {
+            lock (firstOrderModelLock)
+            {
+                string finalFile = "Commands\\Edit\\final.mp4";
+                string picFile = "Commands\\Edit\\first-order-model\\content\\gdrive\\My Drive\\first-order-motion-model\\02.png";
+
+                Bitmap resized = new Bitmap(bmp, new Size(256, 256));
+
+                if (File.Exists(picFile))
+                    File.Delete(picFile);
+
+                resized.Save(picFile);
+
+                if (File.Exists(finalFile))
+                    File.Delete(finalFile);
+
+                Process runner = Process.Start("python.exe", 
+                    "Commands\\Edit\\first-order-model\\executer.py");
+
+                bmp.Dispose();
+                resized.Dispose();
+
+                runner.WaitForExit();
+
+                if (!File.Exists(finalFile))
+                    throw new Exception("Couldn't get enough VRAM to run the model D:");
+                else
+                    DiscordNETWrapper.SendFile(finalFile, m.Channel).Wait();
             }
         }
 
