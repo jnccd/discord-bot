cd6665b8d88bd4a9091060fea9a5a4050cd4b584
jnccd <kobidogao@outlook.com>
2020-08-10 01:50:02 +0200

    Teach MEE7 facts and LOGIC

--- MEE7-Discord-Bot/Commands/Edit/FunctionalEditCommands.cs
+++ MEE7-Discord-Bot/Commands/Edit/FunctionalEditCommands.cs
@@ -43,8 +43,31 @@ namespace MEE7.Commands.Edit
         public string EDesc = "E";
         public double E(EditNull n, IMessage m) => Math.E;
 
-        public string newVectorDesc = "Creates a new Vector object";
-        public Vector2 NewVector(EditNull n, IMessage m, double a, double b) => new Vector2((float)a, (float)b);
+
+        public string GtDesc = "Greater than";
+        public bool Gt(EditNull n, IMessage m, double a, double b) => a > b;
+
+        public string LtDesc = "Lower than";
+        public bool Lt(EditNull n, IMessage m, double a, double b) => a < b;
+
+        public string GeqDesc = "Greater than or equal";
+        public bool Geq(EditNull n, IMessage m, double a, double b) => a >= b;
+
+        public string LeqDesc = "Lower than or equal";
+        public bool Leq(EditNull n, IMessage m, double a, double b) => a <= b;
+
+        public string EqDesc = "Equal";
+        public bool Eq(EditNull n, IMessage m, double a, double b) => a == b;
+
+        public string AndDesc = "And";
+        public bool And(EditNull n, IMessage m, bool a, bool b) => a & b;
+
+        public string OrDesc = "And";
+        public bool Or(EditNull n, IMessage m, bool a, bool b) => a | b;
+
+
+        public string vectorDesc = "Creates a new Vector object";
+        public Vector2 Vector(EditNull n, IMessage m, double a, double b) => new Vector2((float)a, (float)b);
 
         public string ForFuncDesc = "foori foori";
         public a[] ForFunc<a>(a o, IMessage m, string pipe, string varName, float startValue, float endValue, float stepWidth)
