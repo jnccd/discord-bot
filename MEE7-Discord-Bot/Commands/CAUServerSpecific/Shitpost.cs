0d3c3e39867e2b9fd10adc3a9ef2041431382397
niklasCarstensen <niklas.carstensen@gmx.de>
2020-08-17 17:56:42 +0200

    Add $shitpost

---
+++ MEE7-Discord-Bot/Commands/CAUServerSpecific/Shitpost.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.WebSocket;
+using MEE7.Backend;
+using MEE7.Backend.HelperFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MEE7.Commands.CAUServerSpecific
+{
+    class Shitpost : Command
+    {
+        IMessageChannel arena;
+
+        public Shitpost() : base("shitpost", "", false, true)
+        {
+            Program.OnConnected += Program_OnConnected;
+        }
+
+        private void Program_OnConnected()
+        {
+            arena = (IMessageChannel)Program.GetChannelFromID(552976757217820693);
+        }
+
+        public override void Execute(IMessage message)
+        {
+            string Content = message.Content.Split(" ").Skip(1).Combine(" ");
+
+            try
+            {
+                if (message.Attachments.Count == 0)
+                    arena.SendMessageAsync(Content);
+                else
+                    arena.SendFileAsync(message.Attachments.First().Url.GetStreamFromUrl(), message.Attachments.First().Filename, Content);
+            }
+            catch { }
+        }
+    }
+}
