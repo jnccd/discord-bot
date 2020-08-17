3ed90de84bcdf20196ec160e457e8a3cb7ec963f
jnccd <kobidogao@outlook.com>
2020-08-17 19:14:09 +0200

    Add Reacter

--- MEE7-Discord-Bot/Commands/Reacter.cs
+++ MEE7-Discord-Bot/Commands/Reacter.cs
@@ -5,11 +5,11 @@ using System;
 
 namespace MEE7.Commands
 {
-    class GeneralKenobi : Command
+    class Reacter : Command
     {
-        Emote kenobi;
+        Emote kenobi, padoru, hentai;
 
-        public GeneralKenobi()
+        public Reacter()
         {
             Program.OnConnected += Program_OnConnected;
             Program.OnNonCommandMessageRecieved += OnNonCommandMessageRecieved;
@@ -18,6 +18,8 @@ namespace MEE7.Commands
         private void Program_OnConnected()
         {
             kenobi = Emote.Parse("<a:general_kenobi:729800823239999498>");
+            padoru = Emote.Parse("<a:padoru:744966861648560277>");
+            hentai = Emote.Parse("<a:FeelsHentaiMan:744966841402916925>");
         }
 
         private void OnNonCommandMessageRecieved(IMessage messageIn)
@@ -28,6 +30,10 @@ namespace MEE7.Commands
 
             if (message.Content.Contains("Hello there", StringComparison.OrdinalIgnoreCase))
                 message.AddReactionAsync(kenobi).Wait();
+            if (message.Content.Contains("Padoru", StringComparison.OrdinalIgnoreCase))
+                message.AddReactionAsync(padoru).Wait();
+            if (message.Content.Contains("Hentai", StringComparison.OrdinalIgnoreCase))
+                message.AddReactionAsync(hentai).Wait();
         }
 
         public override void Execute(IMessage message)
