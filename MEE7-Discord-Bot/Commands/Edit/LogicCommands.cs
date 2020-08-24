5480aaa7dac63b1ea20937403e53029619d62ae2
jnccd <kobidogao@outlook.com>
2020-08-24 16:03:06 +0200

    Update logic

--- MEE7-Discord-Bot/Commands/Edit/LogicCommands.cs
+++ MEE7-Discord-Bot/Commands/Edit/LogicCommands.cs
@@ -8,13 +8,13 @@ namespace MEE7.Commands.Edit
         public string GtDesc = "Greater than";
         public bool Gt(EditNull n, IMessage m, double a, double b) => a > b;
 
-        public string LtDesc = "Lower than";
+        public string LtDesc = "Less than";
         public bool Lt(EditNull n, IMessage m, double a, double b) => a < b;
 
         public string GeqDesc = "Greater than or equal";
         public bool Geq(EditNull n, IMessage m, double a, double b) => a >= b;
 
-        public string LeqDesc = "Lower than or equal";
+        public string LeqDesc = "Less than or equal";
         public bool Leq(EditNull n, IMessage m, double a, double b) => a <= b;
 
         public string EqDesc = "Equal";
@@ -23,7 +23,7 @@ namespace MEE7.Commands.Edit
         public string AndDesc = "And";
         public bool And(EditNull n, IMessage m, bool a, bool b) => a & b;
 
-        public string OrDesc = "And";
+        public string OrDesc = "Or";
         public bool Or(EditNull n, IMessage m, bool a, bool b) => a | b;
     }
 }
