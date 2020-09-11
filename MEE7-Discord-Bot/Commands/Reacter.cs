80625d1c2b2d9a6854f4087264e5e965e62e5dd0
niklasCarstensen <niklas.carstensen@gmx.de>
2020-09-11 13:18:39 +0200

    Hotel? Trivago

--- MEE7-Discord-Bot/Commands/Reacter.cs
+++ MEE7-Discord-Bot/Commands/Reacter.cs
@@ -2,12 +2,13 @@
 using Discord.WebSocket;
 using MEE7.Backend;
 using System;
+using System.Collections.Generic;
 
 namespace MEE7.Commands
 {
     class Reacter : Command
     {
-        IEmote kenobi, padoru, hentai, eyes, sosig;
+        readonly Dictionary<string, IEmote> emoteDict = new Dictionary<string, IEmote>();
 
         public Reacter()
         {
@@ -17,11 +18,15 @@ namespace MEE7.Commands
 
         private void Program_OnConnected()
         {
-            kenobi = Emote.Parse("<a:general_kenobi:729800823239999498>");
-            padoru = Emote.Parse("<a:padoru:744966861648560277>");
-            hentai = Emote.Parse("<a:FeelsHentaiMan:744966841402916925>");
-            eyes = new Emoji("👀");
-            sosig = Emote.Parse("<a:sosig:746026002119131308>");
+            emoteDict.Add("kenobi", Emote.Parse("<a:general_kenobi:729800823239999498>"));
+            emoteDict.Add("padoru", Emote.Parse("<a:padoru:744966861648560277>"));
+            emoteDict.Add("hentai", Emote.Parse("<a:FeelsHentaiMan:744966841402916925>"));
+            emoteDict.Add("sosig", Emote.Parse("<a:sosig:746026002119131308>"));
+
+            emoteDict.Add("eyes", new Emoji("👀"));
+
+            for (int i = 'A'; i <= 'Z'; i++)
+                emoteDict.Add(((char)i).ToString(), new Emoji(char.ConvertFromUtf32(0x1F1E6 + i - 'A')));
         }
 
         private void OnNonCommandMessageRecieved(IMessage messageIn)
@@ -31,15 +36,24 @@ namespace MEE7.Commands
             var message = messageIn as SocketMessage;
 
             if (message.Content.Contains("Hello there", StringComparison.OrdinalIgnoreCase))
-                message.AddReactionAsync(kenobi).Wait();
+                message.AddReactionAsync(emoteDict.GetValueOrDefault("kenobi")).Wait();
             if (message.Content.Contains("Padoru", StringComparison.OrdinalIgnoreCase))
-                message.AddReactionAsync(padoru).Wait();
+                message.AddReactionAsync(emoteDict.GetValueOrDefault("padoru")).Wait();
             if (message.Content.Contains("Hentai", StringComparison.OrdinalIgnoreCase))
-                message.AddReactionAsync(hentai).Wait();
+                message.AddReactionAsync(emoteDict.GetValueOrDefault("hentai")).Wait();
             if (message.Content.Contains("I saw that", StringComparison.OrdinalIgnoreCase))
-                message.AddReactionAsync(eyes).Wait();
+                message.AddReactionAsync(emoteDict.GetValueOrDefault("eyes")).Wait();
             if (message.Content.Contains("the sauce", StringComparison.OrdinalIgnoreCase))
-                message.AddReactionAsync(sosig).Wait();
+                message.AddReactionAsync(emoteDict.GetValueOrDefault("sosig")).Wait();
+            if (message.Content.Contains("Hotel", StringComparison.OrdinalIgnoreCase))
+                (message as IUserMessage).AddReactionsAsync(new IEmote[] { 
+                    emoteDict.GetValueOrDefault("T"),
+                    emoteDict.GetValueOrDefault("R"),
+                    emoteDict.GetValueOrDefault("I"),
+                    emoteDict.GetValueOrDefault("V"),
+                    emoteDict.GetValueOrDefault("A"),
+                    emoteDict.GetValueOrDefault("G"),
+                    emoteDict.GetValueOrDefault("O"), }).Wait();
         }
 
         public override void Execute(IMessage message) { }
