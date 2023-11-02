0ba23cb1bfc40c91cc0869390b137d61f51a1688
Niklas Carstensen <kobidogao@yahoo.com>
2023-11-02 03:01:13 +0100

    Add startup script

---
+++ update_and_start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+git pull
+
+cd ./MEE7-Discord-Bot
+
+dotnet restore
+dotnet run #-c Release
