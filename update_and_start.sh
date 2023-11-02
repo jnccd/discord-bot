6b20c6b2f994d7b5713e8b56feaca4eb13951c38
jnccd <kobidogao@outlook.com>
2023-11-02 03:44:26 +0100

    Add sleepy time

--- update_and_start.sh
+++ update_and_start.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
+# Get some sleepy time c:
+SLEEP_TIME=0
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -s|--sleep)
+      SLEEP_TIME=$2
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+sleep $SLEEP_TIME
+
+# Get to the right place
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
+cd $SCRIPT_DIR/MEE7-Discord-Bot
 
+# Work
 git pull
-
-cd ./MEE7-Discord-Bot
-
 dotnet restore
 dotnet run -c Release
