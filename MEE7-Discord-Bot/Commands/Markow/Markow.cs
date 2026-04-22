d36b1af7f5dfad62d8a7996c5b912cf4813091f3
Dobiko <kobidogao@yahoo.com>
2026-04-23 00:06:19 +0200

    Disable Markow to lower ram consumption

--- MEE7-Discord-Bot/Commands/Markow/Markow.cs
+++ MEE7-Discord-Bot/Commands/Markow/Markow.cs
@@ -8,103 +8,102 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace MEE7.Commands
-{
-    public class Markow : Command
-    {
-        bool loadedDict;
-        readonly string saucePath = $"Commands{Path.DirectorySeparatorChar}MarkowSources{Path.DirectorySeparatorChar}";
+namespace MEE7.Commands;
 
-        public Markow() : base("markow", "Generates text", false)
-        {
-            Program.OnConnected += OnConnected;
-            Program.OnNonCommandMessageRecieved += OnNonCommandMessageRecieved;
-            Program.OnExit += OnExit;
-        }
+//public class Markow : Command
+//{
+//    bool loadedDict;
+//    readonly string saucePath = $"Commands{Path.DirectorySeparatorChar}MarkowSources{Path.DirectorySeparatorChar}";
 
-        public void OnConnected()
-        {
-            DateTime start = DateTime.Now;
-            string input = "";
+//    public Markow() : base("markow", "Generates text", false)
+//    {
+//        Program.OnConnected += OnConnected;
+//        Program.OnNonCommandMessageRecieved += OnNonCommandMessageRecieved;
+//        Program.OnExit += OnExit;
+//    }
 
-            MarkovHelper.LoadDict();
+//    public void OnConnected()
+//    {
+//        DateTime start = DateTime.Now;
+//        string input = "";
 
-            // Check for reset
-            if (!MarkovHelper.SaveFileExists())
-            {
-                Config.Data.LoadedMarkovTextFiles.Clear();
+//        MarkovHelper.LoadDict();
 
-                // Load from Discord
-                foreach (SocketGuild guild in Program.GetGuilds())
-                    if (guild.Id != 473991188974927882)
-                        foreach (SocketChannel channel in guild.Channels)
-                            if (channel.GetType().GetInterfaces().Contains(typeof(ISocketMessageChannel)))
-                                try
-                                {
-                                    IEnumerable<IMessage> messages = ((ISocketMessageChannel)channel).GetMessagesAsync().FlattenAsync().Result;
-                                    foreach (IMessage m in messages)
-                                        if (!m.Author.IsBot && !string.IsNullOrWhiteSpace(m.Content) && !m.Content.StartsWith(Program.Prefix) && m.Content[0] != '!')
-                                            input += m.Content + "\n";
-                                }
-                                catch { }
-            }
+//        // Check for reset
+//        if (!MarkovHelper.SaveFileExists())
+//        {
+//            Config.Data.LoadedMarkovTextFiles.Clear();
 
-            // Load from text Files
-            if (Directory.Exists(saucePath))
-            {
-                string[] files = Directory.GetFiles(saucePath);
-                foreach (string file in files)
-                {
-                    if (!Config.Data.LoadedMarkovTextFiles.Contains(Path.GetFileName(file)))
-                    {
-                        string[] lines = File.ReadAllLines(file);
-                        foreach (string line in lines)
-                        {
-                            string trimmed = line.Trim('\n').Trim('\t').Trim(' ');
-                            if (!string.IsNullOrWhiteSpace(trimmed))
-                                input += trimmed + "\n";
-                        }
-                        Config.Data.LoadedMarkovTextFiles.Add(Path.GetFileName(file));
-                    }
-                }
-            }
+//            // Load from Discord
+//            foreach (SocketGuild guild in Program.GetGuilds())
+//                if (guild.Id != 473991188974927882)
+//                    foreach (SocketChannel channel in guild.Channels)
+//                        if (channel.GetType().GetInterfaces().Contains(typeof(ISocketMessageChannel)))
+//                            try
+//                            {
+//                                IEnumerable<IMessage> messages = ((ISocketMessageChannel)channel).GetMessagesAsync().FlattenAsync().Result;
+//                                foreach (IMessage m in messages)
+//                                    if (!m.Author.IsBot && !string.IsNullOrWhiteSpace(m.Content) && !m.Content.StartsWith(Program.Prefix) && m.Content[0] != '!')
+//                                        input += m.Content + "\n";
+//                            }
+//                            catch { }
+//        }
 
-            MarkovHelper.AddToDict(input);
+//        // Load from text Files
+//        if (Directory.Exists(saucePath))
+//        {
+//            string[] files = Directory.GetFiles(saucePath);
+//            foreach (string file in files)
+//            {
+//                if (!Config.Data.LoadedMarkovTextFiles.Contains(Path.GetFileName(file)))
+//                {
+//                    string[] lines = File.ReadAllLines(file);
+//                    foreach (string line in lines)
+//                    {
+//                        string trimmed = line.Trim('\n').Trim('\t').Trim(' ');
+//                        if (!string.IsNullOrWhiteSpace(trimmed))
+//                            input += trimmed + "\n";
+//                    }
+//                    Config.Data.LoadedMarkovTextFiles.Add(Path.GetFileName(file));
+//                }
+//            }
+//        }
 
-            loadedDict = true;
-            ConsoleWrapper.WriteLine("Loaded markow in " + (DateTime.Now - start).TotalSeconds + "s", ConsoleColor.Cyan);
-        }
-        public void OnNonCommandMessageRecieved(IMessage message)
-        {
-            try
-            {
-                IEnumerable<IMessage> messages = message.Channel.GetMessagesAsync(2).FlattenAsync().Result;
-                if (messages.Count() == 2)
-                    MarkovHelper.AddToDict(messages.ElementAt(1).Content, message.Content);
-                else
-                    MarkovHelper.AddToDict(message.Content);
-            }
-            catch { }
-        }
-        public void OnExit()
-        {
-            if (loadedDict)
-                MarkovHelper.SaveDict();
-        }
+//        MarkovHelper.AddToDict(input);
 
-        public override void Execute(IMessage message)
-        {
-            string[] split = message.Content.Split(new char[] { ' ', '\n' });
+//        loadedDict = true;
+//        ConsoleWrapper.WriteLine("Loaded markow in " + (DateTime.Now - start).TotalSeconds + "s", ConsoleColor.Cyan);
+//    }
+//    public void OnNonCommandMessageRecieved(IMessage message)
+//    {
+//        try
+//        {
+//            IEnumerable<IMessage> messages = message.Channel.GetMessagesAsync(2).FlattenAsync().Result;
+//            if (messages.Count() == 2)
+//                MarkovHelper.AddToDict(messages.ElementAt(1).Content, message.Content);
+//            else
+//                MarkovHelper.AddToDict(message.Content);
+//        }
+//        catch { }
+//    }
+//    public void OnExit()
+//    {
+//        if (loadedDict)
+//            MarkovHelper.SaveDict();
+//    }
 
-            try
-            {
-                string output = MarkovHelper.GetString(split.Length > 1 ? split.Skip(1).Aggregate((x, y) => { return x + " " + y; }) : "", 5, 2000);
-                DiscordNETWrapper.SendText(output, message.Channel).Wait();
-            }
-            catch (NoEmptyElementException)
-            {
-                DiscordNETWrapper.SendText("Markow isn't ready yet!", message.Channel).Wait();
-            }
-        }
-    }
-}
+//    public override void Execute(IMessage message)
+//    {
+//        string[] split = message.Content.Split(new char[] { ' ', '\n' });
+
+//        try
+//        {
+//            string output = MarkovHelper.GetString(split.Length > 1 ? split.Skip(1).Aggregate((x, y) => { return x + " " + y; }) : "", 5, 2000);
+//            DiscordNETWrapper.SendText(output, message.Channel).Wait();
+//        }
+//        catch (NoEmptyElementException)
+//        {
+//            DiscordNETWrapper.SendText("Markow isn't ready yet!", message.Channel).Wait();
+//        }
+//    }
+//}
